016634a9dfcf03dd62025e5020dcbd2647818030
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-03-15 16:04:54 +0300

    Add project files.

---
+++ KisiselVlog/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KisiselVlog.Models.Entity;
+using KisiselVlog.Models.MyClass;
+
+namespace KisiselVlog.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        KisiselBlogDBEntities db= new KisiselBlogDBEntities();
+        public ActionResult Index()
+        {
+            Class1 cs= new Class1();
+            cs.Deger1 = db.TBLHAKKIMDA.ToList();
+            cs.Deger2 = db.TBLILETISIM.ToList();
+            return View(cs);
+        }
+    }
+}
---
+++ KisiselVlog/Models/MyClass/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KisiselVlog.Models.Entity;
+using KisiselVlog.Models.MyClass;
+
+namespace KisiselVlog.Models.MyClass
+{
+    public class Class1
+    {
+        public IEnumerable<TBLHAKKIMDA> Deger1 { get; set; }
+        public IEnumerable<TBLILETISIM> Deger2 { get; set; }
+
+    }
+}
